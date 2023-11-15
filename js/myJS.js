43055ab92175e7def1c3c709460ffe52bdb649a4
Lydia <dl4519@nyu.edu>
2023-11-16 04:54:19 +0800

    1116-8 proj

--- js/myJS.js
+++ js/myJS.js
@@ -6,11 +6,30 @@ $(document).ready(function(){
     const remained = document.getElementById('remained')
 
     var skillVal = 0;
-    var workPos0 = $('.workSt').offset().top;
-    var workPos = workPos0-200;
+    // var workPos0 = $('.workSt').offset().top;
+    // var workPos = workPos0-200;
     var selfIn0 = $('.textCTN').offset().top;
     var selfIn = selfIn0-200;
 
+    var nav_offset_top = $('header').height() + 50; 
+    /*-------------------------------------------------------------------------------
+	  Navbar 
+	-------------------------------------------------------------------------------*/
+
+	//* Navbar Fixed  
+    function navbarFixed(){
+        if ( $('.header_area').length ){ 
+            $(window).scroll(function() {
+                var scroll = $(window).scrollTop();   
+                if (scroll >= nav_offset_top ) {
+                    $(".header_area").addClass("navbar_fixed");
+                } else {
+                    $(".header_area").removeClass("navbar_fixed");
+                }
+            });
+        };
+    };
+    navbarFixed();
 
     //show self-intro text
     $(".textCTN").hide();
@@ -18,116 +37,107 @@ $(document).ready(function(){
 
     
     //scroll to show gallery
-    $(window).scroll(function(){
+    // $(window).scroll(function(){
 
-        if($(window).scrollTop() > workPos){
-            $(".intro").css("background-color","rgba(220,234,225,0.7)");
-            $(".introWrap").fadeIn(1000);
-        }
-        else{
+    //     if($(window).scrollTop() > workPos){
+    //         $(".intro").css("background-color","rgba(220,234,225,0.7)");
+    //         $(".introWrap").fadeIn(1000);
+    //     }
+    //     else{
 
-            $(".introWrap").hide();
-            // $("#sketch-holder").hide();
+    //         $(".introWrap").hide();
+    //         // $("#sketch-holder").hide();
 
-        }
+    //     }
+
+    // });
+
+
+    // portfolio_filter
+        // custom select variables
+    const select = document.querySelector("[data-select]");
+    const selectItems = document.querySelectorAll("[data-select-item]");
+    const selectValue = document.querySelector("[data-selecct-value]");
+    const filterBtn = document.querySelectorAll("[data-filter-btn]");
+
+    select.addEventListener("click", function () { elementToggleFunc(this); });
+
+    // add event in all select items
+    for (let i = 0; i < selectItems.length; i++) {
+    selectItems[i].addEventListener("click", function () {
+
+        let selectedValue = this.innerText.toLowerCase();
+        selectValue.innerText = this.innerText;
+        elementToggleFunc(select);
+        filterFunc(selectedValue);
 
     });
-
-    //click to view skill percentage
-    $(".c1").click(function(){
-        skillVal = 0.6;
-    });
-
-    $(".c2").click(function(){
-        skillVal = 0.7;
-    });
-
-    $(".c3").click(function(){
-        skillVal = 0.6;
-    });
-
-    $(".c4").click(function(){
-        skillVal = 0.5;
-    });
-
-
-    $(".c5").click(function(){
-        skillVal = 0.8;
-    });
-
-    $(".c6").click(function(){
-        skillVal = 0.9;
-    });
-
-
-    updateBigCup()
-
-    smallCups.forEach((cup,idx)=>{
-        cup.addEventListener('click',()=>highlightCups(idx))
-    })
-
-    function highlightCups(idx){
-        if(idx  == 6 && smallCups[idx].classList.contains("full")){
-            idx--
-        }else if(smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')){
-            idx--
-        }
-            
-        
-         smallCups.forEach((cup,index)=>{
-             if(index <= idx){
-                cup.classList.add('full')
-             }else{
-                 cup.classList.remove('full')
-             }
-         })   
-         
-         updateBigCup()
     }
-    function updateBigCup(){
-        const fullCups = document.querySelectorAll('.cup-small.full').length
-        const totalCups = smallCups.length
 
-        if(fullCups == 0){
-            percentage.style.visibility = 'hidden'
-            percentage.style.height = 0
-        }else{
-            percentage.style.visibility = 'visible'
-            // percentage.style.height = `${fullCups/totalCups*300}px`
-            // percentage.innerText = `${fullCups/totalCups*100}%`
-            percentage.style.height = `${skillVal*300}px`
-            percentage.innerText = `${skillVal*100}%`
+    //skill
+    // $('.imageGallery1 .light').simpleLightbox();
+	
+	// $('.counter').counterUp({
+	// 	delay: 10,
+	// 	time: 1000
+	// });
+	
+	$(".skill_main").each(function() {
+        $(this).waypoint(function() {
+            var progressBar = $(".progress-bar");
+            progressBar.each(function(indx){
+                $(this).css("width", $(this).attr("aria-valuenow") + "%")
+            })
+        }, {
+            triggerOnce: true,
+            offset: 'bottom-in-view'
+
+        });
+    });
+
+    // filter variables
+    const filterItems = document.querySelectorAll("[data-filter-item]");
+
+    const filterFunc = function (selectedValue) {
+        for (let i = 0; i < filterItems.length; i++) {
+          // Split the data-category attribute into an array of categories using a comma
+          const categories = filterItems[i].dataset.category.split(',');
+      
+          // Trim each category to remove any leading or trailing spaces
+          categories.forEach((category, index, array) => {
+            array[index] = category.trim();
+          });
+      
+        //   console.log('Categories:', categories);
+      
+          if (selectedValue === "all") {
+            filterItems[i].classList.add("active");
+          } else if (categories.includes(selectedValue)) {
+            filterItems[i].classList.add("active");
+          } else {
+            filterItems[i].classList.remove("active");
+          }
         }
+      }
+      
 
-        // if(fullCups == totalCups){
-        //     remained.style.visibility = 'hidden'
-        //     remained.style.height = 0
-        // }else{
-        //     remained.style.visibility = 'visible'
-        //     liters.innerText = `${100-(100*skillVal)}%`
-        // }
-        remained.style.visibility = 'visible'
-        // liters.innerText = `${100-(100*skillVal)}%`
-    }
-    //skill ends
+    // add event in all filter button items for large screen
+    let lastClickedBtn = filterBtn[0];
 
-    //click to see ml5 demo
-    $(".btnC").click(function(){
+    for (let i = 0; i < filterBtn.length; i++) {
 
-        // $("#sketch-holder").fadeIn(1000);
-        // window.setup = setup;
-        // window.draw  = draw;
-        // new p5();
-        // $.getScript("js/sketch.js");
-        var script = document.createElement('script');
+    filterBtn[i].addEventListener("click", function () {
 
-        script.type = 'text/javascript';
+        let selectedValue = this.innerText.toLowerCase();
+        selectValue.innerText = this.innerText;
+        filterFunc(selectedValue);
 
-        // script.src = 'a.js?_dc=' + new Date().getTime();
-        script.src = 'js/sketch.js';
-        document.getElementsByTagName('head')[0].appendChild(script);
-
+        lastClickedBtn.classList.remove("active");
+        this.classList.add("active");
+        lastClickedBtn = this;
 
     });
+
+    }
 
 });
